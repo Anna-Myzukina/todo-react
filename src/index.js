cbbdd3b42c2d0e4530a8fbc26683abecc9af4e14
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-03-02 17:25:05 +0200

    Initialize gitrepo, added main files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const TodoList = () => {
+    return (
+    <ul>
+        <li>Learn Hoe to create React</li>
+        <li>Build Awesome App</li>
+    </ul>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const AppHeader = () => {
+    return (
+        <h1>My Todo list</h1>
+    );
+};
+
+const el = (
+<div>
+    <AppHeader />
+    <input placeholder="search"/>
+    <TodoList />
+</div>
+);
+
+ReactDom.render(el, document.getElementById('root'));
